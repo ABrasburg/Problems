355f0368bc19ffdcc980eeb2b847741d7bbf6382
ABrasburg <abrasburg@fi.uba.ar>
2026-05-22 16:11:32 -0300

    Resuelve problema 28

--- problems/strings-parsing/028_text_justification.py
+++ problems/strings-parsing/028_text_justification.py
@@ -3,10 +3,41 @@
 # Given a sequence of words and an integer line length k, return a list of
 # fully justified strings.
 
+def group_words(words, k):
+    packs = [[]]
+    line_len = 0
+    for word in words:
+        extra_space = 0 if not packs[-1] else 1
+        if line_len + extra_space + len(word) <= k:
+            line_len += extra_space + len(word)
+            packs[-1].append(word)
+            continue
+        line_len = len(word)
+        packs.append([word])
+    return packs
+
 
 def justify_text(words, k):
-    pass
-
+    matrix = group_words(words, k)
+    result = []
+    for line in matrix:
+        spaces_to_place = k-sum(len(word) for word in line)
+        if len(line) == 1:
+            result.append(line[0] + " " * spaces_to_place)
+            continue
+        actual = ""
+        spaces = spaces_to_place // (len(line) - 1)
+        for i, word in enumerate(line):
+            if i == len(line)-1:
+                actual += word
+                continue
+            if spaces_to_place % (len(line) - 1) == 0:
+                actual += word + " " * spaces
+            else:
+                actual += word + " " * (spaces + 1)
+                spaces_to_place -= 1
+        result.append(actual)
+    return result
 
 if __name__ == "__main__":
     result = justify_text(
@@ -20,3 +51,5 @@ if __name__ == "__main__":
     ]
 
     assert justify_text(["hello"], 8) == ["hello   "]
+
+    assert justify_text(["a", "b", "a"], 5) == ["a b a"]
